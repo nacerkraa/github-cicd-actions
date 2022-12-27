b9a1f2843f90d9fe66c8ae4d9fd279f0fc100ac4
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-12-27 12:19:56 +0100

    add restController

--- src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,10 +2,15 @@ package com.example.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GithubCicdActionsApplication {
 
+    @GetMapping("/message")
+    public String index(){
+        return "This a message from Application";
+    }
     public static void main(String[] args) {
         SpringApplication.run(GithubCicdActionsApplication.class, args);
     }
